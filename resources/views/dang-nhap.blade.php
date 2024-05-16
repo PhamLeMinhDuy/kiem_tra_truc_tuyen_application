eed98ce72483ad58fcdcb61bfd29998b15a77a52
Phạm Lê Minh Duy <121998401+PhamLeMinhDuy@users.noreply.github.com>
2024-05-16 17:19:50 +0700

    Update dang-nhap.blade.php

--- resources/views/dang-nhap.blade.php
+++ resources/views/dang-nhap.blade.php
@@ -5,8 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-    <script src="{{ mix('/js/app.js') }}"></script>
     <title>Đăng nhập</title>
 </head>
 <body>
